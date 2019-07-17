c3be1d5738149df3dd713fc243fb67b025507759
ZiwenZhuang <leo416850130@outlook.com>
2019-07-17 06:45:31 -0400

    add deformable object

--- multiworld/__init__.py
+++ multiworld/__init__.py
@@ -1,9 +1,11 @@
 from multiworld.envs.mujoco import register_mujoco_envs
 from multiworld.envs.pygame import register_pygame_envs
 from multiworld.envs.goal_env_ext import register_rope_envs
+from multiworld.dfm_env import register_dfm_envs
 
 
 def register_all_envs():
     register_mujoco_envs()    
     register_pygame_envs()
     register_rope_envs()
+    register_dfm_envs()
